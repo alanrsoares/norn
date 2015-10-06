a88d1a614d3d6a196430bb022441008301a431fd
Alan R. Soares <alanrsoars@gmail.com>
2015-10-07 12:58:40 +1300

    Make it so ༼ つ ◕_◕ ༽つ

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+const APPLICATION_NAME = 'Norn'
+const HTTP_PORT = 3000
+
+
+app.get('/', (req, res) => res.end('<h1>Hello, Norn!</h1>'))
+
+app.listen(HTTP_PORT)
+
+console.log(`${APPLICATION_NAME} running on port ${HTTP_PORT}!`)
